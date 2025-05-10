436abcaf961f559347bd0e3959ea88cc352b93aa
Ryan Wessling da Silva <contato@legacysistemas.com>
2025-05-09 23:01:26 -0300

    Consume API

--- converter/views.py
+++ converter/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+import json
+import requests
 
 def converter(request):
-    return render(request, 'converter/home.html')
+    access_key = '28807aceea1acfc131eafe663afdb3e8'
+    url = f'http://api.exchangerate.host/live?access_key={access_key}'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        data_json = json.dumps(data)
+        return HttpResponse(data_json)
+    else:
+        return HttpResponse('Não foi possível acessar a API externa')
